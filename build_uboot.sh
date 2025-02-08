abd0fac2297af2a5aa643c7f2f67de5e47faf224
uvham521 <uvham521@outlook.com>
2025-02-08 08:14:25 +0000

    first commit

---
+++ build_uboot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+build_path="${script_path}/build"
+output_path="${script_path}/output"
+
+export CROSS_COMPILE=aarch64-linux-gnu-
+export BL31=${script_path}/downloads/rk3568_bl31_v1.44.elf
+export ROCKCHIP_TPL=${script_path}/downloads/rk3566_ddr_1056MHz_v1.23.bin
+
+cd ${build_path}/u-boot
+
+make distclean
+make lckfb-tspi-rk3566_defconfig
+make -j`getconf _NPROCESSORS_ONLN`
+
+rm -rf ${output_path}/u-boot
+mkdir -p ${output_path}/u-boot
+
+make savedefconfig
+mv defconfig ${output_path}/u-boot/lckfb-tspi-rk3566_defconfig
+mv u-boot-rockchip.bin ${output_path}/u-boot/lckfb-tspi-rk3566.uboot
